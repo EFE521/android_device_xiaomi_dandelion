b19998f552b72e0ecef536bd5d4d7117742cf904
EFE521 <137198431+EFE521@users.noreply.github.com>
2026-05-06 08:18:46 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,6 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_dandelion.mk
+    $(LOCAL_DIR)/twrp_dandelion.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_dandelion-user \
-    omni_dandelion-userdebug \
-    omni_dandelion-eng
+    twrp_dandelion-eng \
+    twrp_dandelion-userdebug
